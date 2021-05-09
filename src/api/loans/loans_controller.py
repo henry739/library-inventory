60a22262df70cb4c1de4400af34ae01beabb9198
Henry <henry.wj.jewell@gmail.com>
2021-05-09 21:08:44 +0100

    add loan schema and validation

--- src/api/loans/loans_controller.py
+++ src/api/loans/loans_controller.py
@@ -3,11 +3,13 @@ from datetime import datetime, timedelta
 
 from flask import jsonify, request, make_response, Response
 from flask_restful import Resource
+from jsonschema import ValidationError
 
 from database.database import db_session
 from model.book import Book
 from model.loan import Loan
 from model.user import User
+from schema.schema_validator import SchemaValidator
 
 logger = logging.Logger(__name__)
 
@@ -18,6 +20,7 @@ class LoansController(Resource):
     """
     def __init__(self):
         self._loan_length = 7
+        self.validator = SchemaValidator("schema/loan.schema.json")
 
     def generate_due_date(self):
         now = datetime.utcnow()
@@ -46,13 +49,24 @@ class LoansController(Resource):
     def post(self) -> Response:
         """
         """
-        # TODO: Validation against schema
+        # Validate
+        try:
+            self.validator.validate(request.json)
+            if "id" in request.json:
+                return make_response("New records should not contain an ID", 400)
+
+        except ValidationError:
+            logger.exception(f"Request body failed validation against JsonSchema")
+            return make_response("Invalid request format", 400)
+
         user_id = request.json.get("user_id")
         book_id = request.json.get("book_id")
         due_date = self.generate_due_date()
 
+        # Ensure the library can support this loan
         loan = Loan(user_id=user_id, book_id=book_id, due_date=due_date)
         if self.is_loan_possible(user_id, book_id):
+            # Persist
             db_session.add(loan)
             db_session.commit()
 
